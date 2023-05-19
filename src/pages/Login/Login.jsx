7dd984d11df7aa858721067c668ed6a0d9b227ec
nazmunnaharnijhum <nazmunnaharnijhum@gmail.com>
2023-05-19 11:37:51 +0600

    firebase config has been hidden

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,11 @@
 
 
 const Login = () => {
+
+  const handleLogin = event => {
+    event.preventDefault();
+  }
+
     return (
         <div className="hero min-h-screen bg-base-200">
   <div className="hero-content flex-col lg:flex-row-reverse">
@@ -13,25 +18,28 @@ const Login = () => {
     </div>
     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
       <div className="card-body">
+        <form onSubmit={handleLogin}>
         <div className="form-control">
         <h1 className="text-4xl font-bold text-center mb-2">Login</h1>
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="text" placeholder="email" className="input input-bordered" />
+          <input type="email" placeholder="email" className="input input-bordered" />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
-          <input type="text" placeholder="password" className="input input-bordered" />
+          <input type="password" placeholder="password" className="input input-bordered" />
           <label className="label">
             <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
           </label>
         </div>
         <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
+          
+          <input className="btn btn-primary" type="submit" value="Login" />
         </div>
+        </form>
       </div>
     </div>
   </div>
