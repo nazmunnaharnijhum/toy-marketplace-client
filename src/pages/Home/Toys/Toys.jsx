54b7f4d9f942431309e025177b0497a833ecc652
nazmunnaharnijhum <nazmunnaharnijhum@gmail.com>
2023-05-19 11:03:30 +0600

    a gallery has been added

--- src/pages/Home/Toys/Toys.jsx
+++ src/pages/Home/Toys/Toys.jsx
@@ -3,7 +3,25 @@
 const Toys = () => {
     return (
         <div>
-            
+            <h2 className="text-4xl font-bold text-center text-teal-500 mt-12 mb-2">Ours Worries Gallery</h2>
+            <p className="font-bold text-center text-gray-500  mb-4">ToyWarriors is an online toy store that offers a diverse range <br /> of toys for children of all ages. Our website provides a seamless shopping <br /> experience with detailed product descriptions, secure transactions, and prompt <br /> delivery services. Explore our extensive collection and <br /> let the adventures begin!</p>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2">
+
+
+        <img className="w-[308px] h-[212px]" src="https://i.ibb.co/Bcr7kKL/spiderman-g6c3f1fe6a-1920.jpg" alt="" />
+        <img className="w-[308px] h-[212px]"  src="https://i.ibb.co/0jTcnYf/groot-super-hero-ga97e1cf71-1920.jpg" alt="" />
+        <img className="w-[308px] h-[212px]" src="https://i.ibb.co/bJw55vp/captain-america-g09949c8b3-1920.jpg" alt="" />
+        <img className="w-[308px] h-[212px]" src="https://i.ibb.co/m50QfRv/star-lord-peter-jason-quill-gf7c65a8a0-1920.jpg" alt="" />
+        <img className="w-[308px] h-[212px]" src="https://i.ibb.co/5M8LSKL/toy-gfda540427-1920.jpg" alt="" />
+        <img className="w-[308px] h-[212px]" src="https://i.ibb.co/mN9zpRb/spiderman-g33b4c8f3f-1920.jpg" alt="" />
+        <img className="w-[308px] h-[212px]" src="https://i.ibb.co/WcM2jfT/cat-g260ff42d6-1920.jpg" alt="" />
+        <img className="w-[308px] h-[212px]" src="https://i.ibb.co/LY83YKB/dr-strange-gafbe2c0c2-1920.jpg" alt="" />
+        <img className="w-[308px] h-[212px]" src="https://i.ibb.co/6F0wyL9/batman-g7b01ed2b0-1920.jpg" alt="" />
+        <img className="w-[308px] h-[212px]" src="https://i.ibb.co/kcYxv76/superman-ga61c3ee44-1920.jpg" alt="" />
+        <img className="w-[308px] h-[212px]" src="https://i.ibb.co/TH6LgFy/small-ge8a2e6a7c-1920.jpg" alt="" />
+        <img className="w-[308px] h-[212px]" src="https://i.ibb.co/8rjJNZp/toys-g17c3014e1-1920.jpg" alt="" />
+
+            </div>
         </div>
     );
 };
